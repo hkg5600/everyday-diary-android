af26cf97bbb49872abac0c33724ca055bc066e01
gohyeonseok <46782752+hkg5600@users.noreply.github.com>
2019-12-17 11:53:48 +0900

    Add DiaryListActivity

---
+++ app/src/main/java/com/example/everyday_diary/ui/diary_list/DiaryListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.everyday_diary.ui.diary_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.everyday_diary.R
+
+class DiaryListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_list)
+    }
+}
