6e35fa82ae8578179f5b45b64579df8397ff8d06
gohyeonseok <46782752+hkg5600@users.noreply.github.com>
2019-12-19 11:54:40 +0900

    Use databinding for writediaryactivity

--- app/src/main/java/com/example/everyday_diary/ui/write_activity/WriteDiaryActivity.kt
+++ app/src/main/java/com/example/everyday_diary/ui/write_activity/WriteDiaryActivity.kt
@@ -3,11 +3,29 @@ package com.example.everyday_diary.ui.write_activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.everyday_diary.R
+import com.example.everyday_diary.base.BaseActivity
+import com.example.everyday_diary.databinding.ActivityWriteDiaryBinding
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class WriteDiaryActivity : AppCompatActivity() {
+class WriteDiaryActivity : BaseActivity<ActivityWriteDiaryBinding, WriteDiaryActivityViewModel>() {
+    override val layoutResourceId = R.layout.activity_write_diary
+    override val viewModel: WriteDiaryActivityViewModel by viewModel()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write_diary)
+    override fun initView() {
+
     }
+
+    override fun initObserver() {
+
+    }
+
+    override fun initListener() {
+
+    }
+
+    override fun initViewModel() {
+        
+    }
+
+
 }
---
+++ app/src/main/java/com/example/everyday_diary/ui/write_activity/WriteDiaryActivityViewModel.kt
@@ -0,0 +1,6 @@
+package com.example.everyday_diary.ui.write_activity
+
+import com.example.everyday_diary.base.BaseViewModel
+
+class WriteDiaryActivityViewModel : BaseViewModel() {
+}
