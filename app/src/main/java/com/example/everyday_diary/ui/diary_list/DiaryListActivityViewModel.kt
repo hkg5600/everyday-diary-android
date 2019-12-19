68fefc1e818295435abd7ef635f3d2f7cfeff857
gohyeonseok <46782752+hkg5600@users.noreply.github.com>
2019-12-19 09:16:00 +0900

    Rename getDiaryByMonth to getDiaryByDate in DiaryService

--- app/src/main/java/com/example/everyday_diary/ui/diary_list/DiaryListActivityViewModel.kt
+++ app/src/main/java/com/example/everyday_diary/ui/diary_list/DiaryListActivityViewModel.kt
@@ -6,5 +6,5 @@ import com.example.everyday_diary.utils.TokenObject
 
 class DiaryListActivityViewModel(private val service: DiaryService) : BaseViewModel() {
 
-    fun getDiaryByMonth(id: Int) = addDisposable(service.getDiaryByMonth(TokenObject.tokenData(), id), getDataObserver())
+    fun getDiaryByMonth(id: Int) = addDisposable(service.getDiaryByDate(TokenObject.tokenData(), id), getDataObserver())
 }
