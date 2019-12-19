cbd55ad1140d419707e11470951c694d16425918
gohyeonseok <46782752+hkg5600@users.noreply.github.com>
2019-12-19 09:08:36 +0900

    Add getDiaryByMonth func to DiaryService

--- app/src/main/java/com/example/everyday_diary/network/service/DiaryService.kt
+++ app/src/main/java/com/example/everyday_diary/network/service/DiaryService.kt
@@ -1,16 +1,17 @@
 package com.example.everyday_diary.network.service
 
 import com.example.everyday_diary.network.api.DiaryApi
+import com.example.everyday_diary.network.response.DiaryListResponse
 import com.example.everyday_diary.network.response.MonthCount
 import com.example.everyday_diary.network.response.Response
 import io.reactivex.Single
 
 interface DiaryService {
-    fun getDiaryCount(token: String, id: Int) : Single<retrofit2.Response<Response<MonthCount>>>
+    fun getDiaryCount(token: String, id: Int): Single<retrofit2.Response<Response<MonthCount>>>
+    fun getDiaryByMonth(token: String, id: Int): Single<retrofit2.Response<Response<DiaryListResponse>>>
 }
 
 class DiaryServiceImpl(private val api: DiaryApi) : DiaryService {
-
+    override fun getDiaryByMonth(token: String, id: Int) = api.getDiaryByMonth(token, "/api/diary/diary-month/$id/")
     override fun getDiaryCount(token: String, id: Int) = api.getDiaryCount(token, "/api/diary/month/$id/")
-
 }
