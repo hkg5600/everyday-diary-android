d4ac73b51561a6f9f841994376855a8dfe8f2e8c
gohyeonseok <46782752+hkg5600@users.noreply.github.com>
2019-12-20 09:41:10 +0900

    Create DiaryDetailActivity

---
+++ app/src/main/java/com/example/everyday_diary/ui/diary_detail/DiaryDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.everyday_diary.ui.diary_detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.everyday_diary.R
+
+class DiaryDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_diary_detail)
+    }
+}
