d5045bc321940d9a4d298b530b6b8e99046a8f81
gohyeonseok <46782752+hkg5600@users.noreply.github.com>
2019-12-20 09:10:23 +0900

    Update SplashActivityViewModel.kt

--- app/src/main/java/com/example/everyday_diary/ui/splash/SplashActivityViewModel.kt
+++ app/src/main/java/com/example/everyday_diary/ui/splash/SplashActivityViewModel.kt
@@ -7,9 +7,12 @@ import com.example.everyday_diary.network.reqeust.TokenRequest
 
 class SplashActivityViewModel(private val userService: UserService) : BaseViewModel() {
 
-    fun verifyToken(token: String) = addDisposable(userService.verifyToken(TokenRequest(token)), getMsgObserver())
+    fun verifyToken(token: String) =
+        addDisposable(userService.verifyToken(TokenRequest(token)), getMsgObserver())
 
-    fun refreshToken(token: String) = addDisposable(userService.refreshToken(TokenRequest(token)), getDataObserver())
+    fun refreshToken(token: String) =
+        addDisposable(userService.refreshToken(TokenRequest(token)), getDataObserver())
 
-    fun insertToken(token: String, refreshToken: String) = insertToken(Token(1, token, refreshToken))
+    fun insertToken(token: String, refreshToken: String) =
+        insertToken(Token(1, token, refreshToken))
 }
