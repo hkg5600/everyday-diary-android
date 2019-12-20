3ab4c48991140ca4b6b33edfb4ad697e1663278a
gohyeonseok <46782752+hkg5600@users.noreply.github.com>
2019-12-20 09:10:15 +0900

    Update MainActivityViewModel.kt

--- app/src/main/java/com/example/everyday_diary/ui/main/MainActivityViewModel.kt
+++ app/src/main/java/com/example/everyday_diary/ui/main/MainActivityViewModel.kt
@@ -5,8 +5,14 @@ import com.example.everyday_diary.network.service.DiaryService
 import com.example.everyday_diary.network.service.UserService
 import com.example.everyday_diary.utils.TokenObject
 
-class MainActivityViewModel(private val diaryService: DiaryService, private val userService: UserService) : BaseViewModel() {
+class MainActivityViewModel(
+    private val diaryService: DiaryService,
+    private val userService: UserService
+) : BaseViewModel() {
 
-    fun getDiaryCount(id: Int) = addDisposable(diaryService.getDiaryCount(TokenObject.tokenData(), id), getDataObserver())
-    fun getUserInfo() = addDisposable(userService.getUserInfo(TokenObject.tokenData()), getDataObserver())
+    fun getDiaryCount(id: Int) =
+        addDisposable(diaryService.getDiaryCount(TokenObject.tokenData(), id), getDataObserver())
+
+    fun getUserInfo() =
+        addDisposable(userService.getUserInfo(TokenObject.tokenData()), getDataObserver())
 }
