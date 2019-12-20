4386a4486f411ffba7869599c670cdb538969a21
gohyeonseok <46782752+hkg5600@users.noreply.github.com>
2019-12-20 09:06:01 +0900

    Update MonthAdapter.kt

--- app/src/main/java/com/example/everyday_diary/adapter/MonthAdapter.kt
+++ app/src/main/java/com/example/everyday_diary/adapter/MonthAdapter.kt
@@ -55,7 +55,7 @@ class MonthAdapter : RecyclerView.Adapter<MonthAdapter.MonthHolder>() {
 
     inner class MonthHolder(private val binding: MonthItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
-        val layout : ConstraintLayout = binding.holderLayout
+        val layout: ConstraintLayout = binding.holderLayout
         val progress: ProgressBar = binding.progressDate
 
         fun bind(item: Month) {
