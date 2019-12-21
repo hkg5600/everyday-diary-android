26de885e712de668489c24bd63b39af08f6cc779
gohyeonseok <46782752+hkg5600@users.noreply.github.com>
2019-12-21 09:43:40 +0900

    Add RecentDiaryActivity

---
+++ app/src/main/java/com/example/everyday_diary/ui/recent_diary/RecentDiaryActivity.kt
@@ -0,0 +1,13 @@
+package com.example.everyday_diary.ui.recent_diary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.everyday_diary.R
+
+class RecentDiaryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recent_diary)
+    }
+}
